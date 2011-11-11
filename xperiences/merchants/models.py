59bf56b7836315a6c385325a8985a574bde932f0
Refael Ackermann <refack@gmail.com>
2011-11-11 21:20:18 +0200

    Restore models

--- xperiences/merchants/models.py
+++ xperiences/merchants/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+﻿from django.db import models
 from django.contrib.localflavor.us.models import * 
 from django.db.models.signals import pre_save  
 from django.template.defaultfilters import slugify 
