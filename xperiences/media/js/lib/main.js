804cf63103a64eb10d27faaeac86c12efdbe4a05
Na'ama Moran <naama.moran@gmail.com>
2011-11-21 03:53:20 -0800

    working on the experience_profile.html template. Added js files for image gallery animation. Still having some problems figuring out how to link the images from S3

---
+++ xperiences/media/js/lib/main.js
@@ -0,0 +1,92 @@
+
+var ANIMATE_INTERVAL = 6000;
+var intervalIndex;
+
+$(document).ready(function() {
+	var mainImg = $("#exp-main-image");
+	var thumbs = $(".exp-thumbs-img");
+	var thumbswrapper = $("#exp-thumbs");
+
+	var thumbW = thumbs.first().width();
+	var thumbH = thumbs.first().height();
+	thumbswrapper.css("width", thumbW * (thumbs.length - 1));
+	thumbswrapper.css("height", thumbH);
+
+	thumbs.each(function(index, el) {
+		$(el).css({
+			"position": "absolute",
+			"left": (index - 1) * thumbW
+		});
+	});
+
+	intervalIndex = window.setInterval(function() {
+		var thumbs = $(".exp-thumbs-img");
+		var first = thumbs.first();
+		var second = first.next();
+		first.remove();
+		first.css({
+			"left": (thumbs.length - 1) * thumbW
+		})
+		var w = thumbW;
+		thumbswrapper.append(first);
+		thumbs.each(function(index, el) {
+			$(el).animate({
+				"left": "-=" + thumbW
+			})
+		});
+		setMainImage(second.prop("src"));
+	}, ANIMATE_INTERVAL);
+
+	thumbs.live("click", function(e) {
+		setImageToMain($(e.currentTarget));
+	})
+
+	setMainImage(thumbs.first().prop("src"));
+});
+
+function setImageToMain(item) {
+	var thumbs = $(".exp-thumbs-img");
+	var index = item.prevAll("img").length;
+	if (index >= thumbs.length) return;
+
+	var thumbswrapper = $("#exp-thumbs");
+	var w = thumbs.first().width();
+
+	var first = thumbs.first();
+	first.remove();
+	first.css({
+		"left": item.css('left')
+	});
+	item.before(first)
+
+	setMainImage(item.prop("src"));	
+
+	item.animate({
+		"opacity": 0
+	}, {
+		"complete": function() {
+			item.remove();
+			thumbswrapper.prepend(item);
+			item.css({
+				"first": -w,
+				"opacity": ""	
+			});
+		}
+	});
+}
+
+function setMainImage(imgPath) {
+	var mainImg = $("#exp-main-image");
+	var oldImage = mainImg.children("img");
+	var newImage = $("<img>").addClass("main-image").prop("src", imgPath);
+	mainImg.prepend(newImage);
+	if(oldImage.length > 0) {
+		oldImage.animate({
+			"opacity": 0	
+		},{
+			"complete": function() {
+				oldImage.remove();
+			}
+		})
+	}
+}
