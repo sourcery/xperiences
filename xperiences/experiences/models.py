3067390296a603e950d43a9491cc1a86a2618d38
Refael Ackermann <refack@gmail.com>
2011-11-22 22:45:56 +0200

    One more fix for images

--- xperiences/experiences/models.py
+++ xperiences/experiences/models.py
@@ -8,12 +8,11 @@ class Experience(models.Model):
     description = models.TextField(max_length=250)
     category = models.CharField(max_length=50)
     location = models.CharField(max_length=100)
-    #picture = models.ImageField(upload_to="/uploads", null=True) #null=True means that picture is not mandatory
     #price = models.TextField(max_length=10)
     price = models.PositiveIntegerField(default=0)
     merchant = models.ForeignKey(Merchant, null=True)
     pub_date = models.DateField(null=True)
-    #photo1 = models.FileField(upload_to='photos', null=True)
+    photo1 = models.FileField(upload_to='%Y%m%d%H%M%S', null=True, blank=True)
     #photo2 = models.FileField(upload_to='photos', null=True)
     #photo3 = models.FileField(upload_to='photos', null=True)
     #photo4 = models.FileField(upload_to='photos', null=True)
