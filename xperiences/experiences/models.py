13783caed3549e48437a3e852e55fb04da99103f
Refael Ackermann <refack@gmail.com>
2011-11-23 07:46:04 +0200

    Restore 5 pictures

--- xperiences/experiences/models.py
+++ xperiences/experiences/models.py
@@ -4,20 +4,18 @@ from django.template.defaultfilters import slugify
 from merchants.models import Merchant
 
 class Experience(models.Model):
-    title = models.CharField(max_length=50)  # by default blank=false and null=false, meaning that both fields are mandatory in both admin and DB
+    title = models.CharField(max_length=50)
     description = models.TextField(max_length=250)
     category = models.CharField(max_length=50)
     location = models.CharField(max_length=100)
-    #price = models.TextField(max_length=10)
     price = models.PositiveIntegerField(default=0)
     merchant = models.ForeignKey(Merchant, null=True)
     pub_date = models.DateField(null=True)
-    photo1 = models.FileField(upload_to='%Y%m%d%H%M%S', null=True, blank=True)
-    #photo2 = models.FileField(upload_to='photos', null=True)
-    #photo3 = models.FileField(upload_to='photos', null=True)
-    #photo4 = models.FileField(upload_to='photos', null=True)
-    #photo5 = models.FileField(upload_to='photos', null=True)
-    # ask Refael why null=True is not working on the admin...
+    photo1 = models.FileField(upload_to='%Y%m%d%H%M%S')
+    photo2 = models.FileField(upload_to='%Y%m%d%H%M%S', null=True, blank=True)
+    photo3 = models.FileField(upload_to='%Y%m%d%H%M%S', null=True, blank=True)
+    photo4 = models.FileField(upload_to='%Y%m%d%H%M%S', null=True, blank=True)
+    photo5 = models.FileField(upload_to='%Y%m%d%H%M%S', null=True, blank=True)
 
 
     @property
