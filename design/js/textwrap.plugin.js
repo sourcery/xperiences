199578b136d5f37677b161d336c63e6c87ba38aa
Chong Han <johncch@gmail.com>
2011-12-01 00:17:52 -0800

    Preliminary work on forms

---
+++ design/js/textwrap.plugin.js
@@ -0,0 +1,41 @@
+(function($){
+	$.fn.defaultVal = function() {
+		var value = this.val();
+		var self = this;
+		if(value) {
+			var parentDiv = $("<div>");
+			parentDiv.addClass("default-val-parent");
+			var valueDiv = $("<div>");
+			valueDiv.addClass("default-val-text").text(value);
+			this.before(parentDiv);
+			this.remove();
+			this.val("");
+			parentDiv.append(this);
+			parentDiv.append(valueDiv);
+
+			valueDiv.on('click', function(e) {
+				self.focus();
+			});
+
+			this.on("focus", function(e) {
+				valueDiv.animate({
+					'width': 0,
+					'height': 0 
+				});
+			});
+
+			var w = this.width();
+			var h = this.height();
+
+			this.on('blur', function(e){
+				var curEl = $(e.currentTarget);
+				if(curEl.val() == '') {
+					valueDiv.animate({
+						'width': w,
+						'height': h
+					});
+				}
+			});
+		}
+	};
+})(jQuery);
