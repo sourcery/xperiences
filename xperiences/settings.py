e75dc9eabeefb47287de5e177d0b8b3695b721f6
Ishai Jaffe <ishai@empeeric.com>
2011-12-05 20:17:28 +0200

    test #2

--- xperiences/settings.py
+++ xperiences/settings.py
@@ -6,7 +6,7 @@ PRODUCTION = 'MONGOLAB_URI' in os.environ
 STAGING = os.environ.get('IS_STAGING') == 'True'
 if STAGING:
     PRODUCTION = False
-STAGING = True
+PRODUCTION = True
 
 BASE_URL = 'http://dev.empeeric.com'
 
