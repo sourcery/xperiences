c9f4d161054ea95146f1659543b5e3a5f8ca93de
Refael Ackermann <refack@gmail.com>
2011-12-10 11:35:09 +0200

    small login fixes

--- xperiences/merchants/urls.py
+++ xperiences/merchants/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('merchants.views',
                        url(r'^register/$', 'register', name='merchant_register'),
                        url(r'^experiences/$', 'experiences', name='merchant_experiences'),
-                       url(r'^edit_experience/(?P<id>[a-f0-9]+)/?$', 'edit_experience', name='merchant_edit_experience'),
+                       url(r'^edit_experience/(?P<slug_id>[a-z0-9\-]+)/?$', 'edit_experience', name='merchant_edit_experience'),
                        url(r'^inbox/$', 'merchant_inbox', name='merchant_inbox'),
                        url(r'^view_message/(?P<id>[a-f0-9]+)/$', 'view_message', name='view_message'),
                        url(r'^comment/(?P<username>[-\w]+)/$', 'comment_merchant', name='comment_merchant'),
