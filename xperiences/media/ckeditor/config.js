f828cac3fe5c1f929a68927f4086c1c1f31e9731
Ishai Jaffe <ishai@empeeric.com>
2011-12-22 18:50:11 +0200

    ckeditor configured

--- xperiences/media/ckeditor/config.js
+++ xperiences/media/ckeditor/config.js
@@ -8,4 +8,12 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+	config.toolbar = [
+   ['Styles','Format','Font','FontSize'],
+   '/',
+   ['Bold','Italic','Underline','StrikeThrough','-','Outdent','Indent','-','Blockquote'],
+   '/',
+   ['NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+   ['Link','TextColor','BGColor']
+	] ;
 };
