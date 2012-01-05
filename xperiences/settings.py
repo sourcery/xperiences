e7f28d86f5dd62ae02e948681c3866506c138075
Ishai Jaffe <ishai@empeeric.com>
2012-01-05 17:27:49 +0200

    removed password ,

--- xperiences/settings.py
+++ xperiences/settings.py
@@ -247,7 +247,7 @@ if KIDS:
 if 'SENDGRID_PASSWORD' in os.environ:
     EMAIL_HOST = 'smtp.sendgrid.net'
     EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD'],
+    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     SERVER_EMAIL = EMAIL_HOST_USER
