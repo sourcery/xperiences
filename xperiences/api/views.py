b00bdee5f44f06a2db76cfa2ef89388d79fce57a
Ishai Jaffe <ishai@empeeric.com>
2012-01-09 15:14:18 +0200

    test url

--- xperiences/api/views.py
+++ xperiences/api/views.py
@@ -1,8 +1,10 @@
 from backend import utils
+from django.http import HttpResponse
 import settings
 
 __author__ = 'ishai'
 
 def test(request):
     utils.send_email_with_template(settings.EMAIL_HOST_USER,['ishaijaffe84@gmail.com'],'xperiences test','empty.html',{'content':request})
+    return HttpResponse('ok')
   
